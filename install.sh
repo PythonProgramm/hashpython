a0b2a41205e052cf4abc2e38add4704b423abb49
PythonProgramm <frederik.kursawe@icloud.com>
2021-01-14 13:33:52 +0100

    Installation file

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo cp MD5.py /usr/share/hashpython/
 sudo cp MD4.py /usr/share/hashpython/
 sudo cp SHA224.py /usr/share/hashpython/
 sudo cp SHA384.py /usr/share/hashpython/
+sudo cp SHA512WHIRLPOOL.py /usr/share/hashpython/
 sudo cp main.py /usr/share/hashpython/
 sudo cp hashpython /bin/
+sudo cp hashid.py /usr/share/hashpython/
 sudo chmod +x /bin/hashpython
